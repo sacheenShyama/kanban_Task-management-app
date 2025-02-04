180a54ba475eb726f78b347ba1dbd3f755b7fbe6
Sachin kumar <93372997+sacheenShyama@users.noreply.github.com>
2025-02-05 05:25:17 +0530

    api deploying successfull

--- api/index.js
+++ api/index.js
@@ -3,19 +3,14 @@ const http = require("http");
 const mongoose = require("mongoose");
 const app = require("./src/app");
 
-// Configuration
 const PORT = process.env.PORT || 8000;
 const MONGO_URL = process.env.MONGO_URL;
 
 mongoose
   .connect(MONGO_URL)
   .then(() => {
-    console.log("Connected to MongoDB");
-
     const server = http.createServer(app);
-    server.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
+    server.listen();
   })
   .catch((err) => {
     console.error("Failed to connect to MongoDB", err);
